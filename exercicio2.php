200f3fee5726e55db4f4742498337051848174d7
Mhs221 <matheusbombomhenrique@gmail.com>
2025-06-04 12:25:26 -0300

    forms

---
+++ exercicio2.php
@@ -0,0 +1,18 @@
+<?php
+
+$nome=$_GET['nm'];
+echo $nome."<br>";
+$textarea=$_GET ['recado'];
+echo $textarea. "<br>";
+$genero=$_GET['genero'];
+echo "seu genero é $genero"."<br>";
+$estado=$_GET['uf'];
+echo "vc mora no estado de $estado";
+$opcao[0]$_GET[op];
+$opcao[1]$_GET[op1];
+$opcao[2]$_GET[op2];
+echo "<br> sua escolhas foram";
+for ($i=0; $i < ; $i++) { 
+    if ($opcao[])
+    echo "<br>".$opcao [1];
+}
